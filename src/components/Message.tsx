a631d24eb66a25779509697351cb7d21a1f28b3e
Aayush Shukla <oreus.aks@gmail.com>
2024-09-09 02:39:19 +0530

    refactor: update UI

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -7,9 +7,11 @@ const Message: React.FC<MessageProps> = ({ text, isUser }) => {
   return (
     <div className={`flex ${isUser ? "justify-end" : "justify-start"} mb-4`}>
       <div
-        className={`${
-          isUser ? "bg-gray-300 text-black" : "bg-gray-200 text-black"
-        } p-3 rounded-lg max-w-xs shadow-sm border border-black`}
+        className={`border ${
+          isUser
+            ? "bg-gray-300 text-black border-gray-800"
+            : "bg-gray-200 text-black border-gray-400"
+        } p-3 rounded-lg max-w-xs shadow-sm`}
       >
         {text}
       </div>
