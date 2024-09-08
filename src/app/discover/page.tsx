fe2e1ed869a92cec8dad8074210a6a1040552f7d
Aayush Shukla <oreus.aks@gmail.com>
2024-09-09 02:11:32 +0530

    feat: decentralized the navbar

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -1,6 +1,7 @@
 import CardWithButton from "@/components/CardWithButton";
 import Chip from "@/components/FilterChip";
 import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
 import SearchBar from "@/components/SearchBar";
 import { exploreData } from "@/data/ExploreData";
 import React from "react";
@@ -8,6 +9,7 @@ import React from "react";
 const Search = () => {
   return (
     <div>
+      <NavBar />
       <div className="flex flex-col items-start w-[80%] mx-auto">
         <h1 className="text-[5rem] font-bold mt-24">Explore</h1>
         <SearchBar />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
 import Dashboard from "@/components/pageCompnents/Dashboard";
 
 export default function Home() {
   return (
     <main>
+      <NavBar />
       <div className=" flex flex-col min-h-screen items-center text-center justify-center">
         <Dashboard />
       </div>
