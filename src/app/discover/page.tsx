d5394b1e961ad59abe2a2bb1e91515b3e84070b2
Aayush Shukla <oreus.aks@gmail.com>
2024-09-08 20:01:24 +0530

    chore: add footer to Discover page

--- src/app/discover/page.tsx
+++ src/app/discover/page.tsx
@@ -1,13 +1,17 @@
 import Chip from "@/components/FilterChip";
+import Footer from "@/components/Footer";
 import SearchBar from "@/components/SearchBar";
 import React from "react";
 
 const Search = () => {
   return (
-    <div className="flex flex-col items-start w-[80%] mx-auto">
-      <h1 className="text-[5rem] font-bold mt-24">Explore</h1>
-      <SearchBar />
-      <Chip />
+    <div>
+      <div className="flex flex-col items-start w-[80%] mx-auto">
+        <h1 className="text-[5rem] font-bold mt-24">Explore</h1>
+        <SearchBar />
+        <Chip />
+      </div>
+      <Footer />
     </div>
   );
 };
