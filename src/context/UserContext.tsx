a50070de5601ba842be9e25d62f043b7cb7ed572
Aayush Shukla <oreus.aks@gmail.com>
2024-09-16 17:38:52 +0530

    feat: create custom useUser hook to transfer context everywhere

---
+++ src/context/UserContext.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { createClient } from "@/utils/supabase/client";
+import { User } from "@supabase/supabase-js";
+
+const UserContext = createContext<
+  | {
+      user: User | null;
+      setUser: React.Dispatch<React.SetStateAction<User | null>>;
+    }
+  | undefined
+>(undefined);
+
+export const UserProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [user, setUser] = useState<User | null>(null);
+  const supabase = createClient();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data, error } = await supabase.auth.getSession();
+      if (data && data.session) {
+        setUser(data.session.user);
+      }
+    };
+
+    fetchUser();
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_, session) => {
+      setUser(session?.user ?? null);
+    });
+
+    return () => {
+      subscription?.unsubscribe();
+    };
+  }, [supabase]);
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error("useUser must be used within a UserProvider");
+  }
+  return context;
+};
