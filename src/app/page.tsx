d69d15f7aed020ef57a715ca3217b2bbe032708b
Aayush Shukla <oreus.aks@gmail.com>
2024-09-16 18:27:29 +0530

    feat: add loading component and segregate User and Freelancer dashboards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,64 @@
 import Footer from "@/components/navigation/Footer";
 import DashboardPublic from "@/components/pageComponents/DashboardPublic";
 import NavBar from "@/components/navigation/NavBar";
-import DashboardPrivate from "@/components/pageComponents/DashboardPrivate";
+import FreelancerDashboard from "@/components/pageComponents/FreelancerDashboard";
 import { useUser } from "@/context/UserContext";
+import EmployerDashboard from "@/components/pageComponents/EmployerDashboard";
+import { useEffect, useState } from "react";
+import { createClient } from "@/utils/supabase/client";
 
 export default function Home() {
   const { user } = useUser();
+  const [role, setRole] = useState<string | undefined>(undefined);
+  const [loading, setLoading] = useState(true); // Added loading state
+  const supabase = createClient();
+
+  useEffect(() => {
+    const fetchRole = async () => {
+      if (user) {
+        const { data, error } = await supabase
+          .from("user")
+          .select("role")
+          .eq("userid", user.id)
+          .single();
+
+        if (error) {
+          console.error(error);
+        } else if (data) {
+          console.log(data);
+          setRole(data.role);
+        }
+        setLoading(false);
+      } else {
+        setLoading(false);
+      }
+    };
+
+    fetchRole();
+  }, [user, supabase]);
+
+  if (loading) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <p className="text-5xl font-bold">FirstCheque</p>
+        <p className="text-2xl mt-2">The gateway to freelancing</p>
+      </div>
+    );
+  }
 
   return (
     <main>
       <NavBar />
-      <div className=" flex flex-col items-center text-center justify-center">
-        {user ? <DashboardPrivate /> : <DashboardPublic />}
+      <div className="flex flex-col items-center text-center justify-center">
+        {user ? (
+          role === "freelancer" ? (
+            <FreelancerDashboard />
+          ) : (
+            <EmployerDashboard />
+          )
+        ) : (
+          <DashboardPublic />
+        )}
       </div>
       <Footer />
     </main>
--- src/components/pageComponents/FreelancerDashboard.tsx
+++ src/components/pageComponents/FreelancerDashboard.tsx
@@ -2,7 +2,7 @@
 import SkillRecomendation from "@/components/recommendation/SkillRecommendations";
 import HistoryRecommendations from "@/components/recommendation/HistoryRecommendation";
 
-const DashboardPrivate = () => {
+const FreelancerDashboard = () => {
   return (
     <div className="bg-white p-32">
       <h1 className="text-6xl text-center font-semibold mb-4">Discover Jobs</h1>
@@ -15,4 +15,4 @@ const DashboardPrivate = () => {
   );
 };
 
-export default DashboardPrivate;
+export default FreelancerDashboard;
