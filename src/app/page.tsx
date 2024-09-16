2ccbb38d6b32c24efd6f20ad7892a93af0f46dee
Aayush Shukla <oreus.aks@gmail.com>
2024-09-16 19:19:43 +0530

    misc: temporarily disabling loading screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,14 @@ import EmployerDashboard from "@/components/pageComponents/EmployerDashboard";
 export default function Home() {
   const { user, role, loading } = useUser();
 
-  if (loading) {
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen">
-        <p className="text-5xl font-bold">FirstCheque</p>
-        <p className="text-2xl mt-2">The gateway to freelancing</p>
-      </div>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <div className="flex flex-col items-center justify-center min-h-screen">
+  //       <p className="text-5xl font-bold">FirstCheque</p>
+  //       <p className="text-2xl mt-2">The gateway to freelancing</p>
+  //     </div>
+  //   );
+  // }
 
   return (
     <main>
--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useUser } from "@/context/UserContext";
 import Button from "../ui/Button";
