ea8380e3571a732d7fc5fb5193d3d2217586d3e7
Aayush Shukla <oreus.aks@gmail.com>
2024-09-21 22:23:05 +0530

    feat: display different options based on role

--- src/components/navigation/NavLink.tsx
+++ src/components/navigation/NavLink.tsx
@@ -1,28 +1,44 @@
 "use client";
 import React from "react";
 import Link from "next/link";
-import { withAuthOptions, withoutAuthOptions } from "@/data/NavList";
+import {
+  withAuthOptionsFreelancer,
+  withAuthOptionsEmployer,
+  withoutAuthOptions,
+} from "@/data/NavList";
 import { User } from "@supabase/supabase-js";
 
 interface NavLinkProps {
   user: User | null;
+  role: string;
 }
 
-const NavLink = ({ user }: NavLinkProps) => {
+const NavLink = ({ user, role }: NavLinkProps) => {
   return (
     <nav>
       <ul className="flex-1 flex justify-center">
         {user
-          ? withAuthOptions.map((option, index) => (
-              <li key={index}>
-                <Link
-                  href={option.route}
-                  className="rounded-lg hover:bg-slate-200 text-lg py-2 px-4 md:bg-transparent font-semibold"
-                >
-                  {option.title}
-                </Link>
-              </li>
-            ))
+          ? role === "freelancer"
+            ? withAuthOptionsFreelancer.map((option, index) => (
+                <li key={index}>
+                  <Link
+                    href={option.route}
+                    className="rounded-lg hover:bg-slate-200 text-lg py-2 px-4 md:bg-transparent font-semibold"
+                  >
+                    {option.title}
+                  </Link>
+                </li>
+              ))
+            : withAuthOptionsEmployer.map((option, index) => (
+                <li key={index}>
+                  <Link
+                    href={option.route}
+                    className="rounded-lg hover:bg-slate-200 text-lg py-2 px-4 md:bg-transparent font-semibold"
+                  >
+                    {option.title}
+                  </Link>
+                </li>
+              ))
           : withoutAuthOptions.map((option, index) => (
               <li key={index}>
                 <Link
